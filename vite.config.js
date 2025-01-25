641d7aa5d6bc4f900d7d3bfa1945a68a0cbbbd4b
Edgar Rivas <webdevedd@gmail.com>
2025-01-24 18:39:39 -0800

    fixed paths for pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+  base: '/LambdaSolutions/',
+});
